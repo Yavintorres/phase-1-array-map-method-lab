654cbdc0febd5fc860e77d4210c2e87f062f69a9
Yavin Torres <yavintorres@gmail.com>
2023-04-09 00:17:09 -0700

    complete lab

--- index.js
+++ index.js
@@ -13,4 +13,10 @@ const tutorials = [
 
 const titleCased = () => {
   return tutorials
+  .map(tutorial => {
+    let words = tutorial.split(' ')
+    let capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    let response = capitalizedWords.join(' ')
+    return response
+  })
 }
